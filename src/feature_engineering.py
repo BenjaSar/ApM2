c41cd1f8e198af22edae7a499484a103ba904fa3
BenjaSar <fsarmiento1805@gmail.com>
2023-07-17 22:53:26 -0300

    Reading data and first part of feature engineering

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -10,6 +10,18 @@ FECHA:
 
 # Imports
 
+import glob
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+import datetime as dt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier, RandomTreesEmbedding
+from scipy import stats
+
+
 class FeatureEngineeringPipeline(object):
 
     def __init__(self, input_path, output_path):
@@ -19,13 +31,16 @@ class FeatureEngineeringPipeline(object):
     def read_data(self) -> pd.DataFrame:
         """
         COMPLETAR DOCSTRING 
-        
         :return pandas_df: The desired DataLake table as a DataFrame
         :rtype: pd.DataFrame
         """
-            
-        # COMPLETAR CON CÓDIGO
-        
+        input_path = '../data'
+
+        data_train = pd.read_csv(input_path + '/Train_BigMart.csv')
+        data_test = pd.read_csv(input_path + '/Test_BigMart.csv')
+        pandas_df = pd.concat([data_train, data_test], ignore_index=True, sort=False)
+        print(pandas_df.head(20))
+
         return pandas_df
 
     
@@ -34,12 +49,12 @@ class FeatureEngineeringPipeline(object):
         COMPLETAR DOCSTRING
         
         """
+        df['Outlet_Establishment_Year'] = 2020 - df['Outlet_Establishment_Year']
         
-        # COMPLETAR CON CÓDIGO
         
         return df_transformed
 
-    def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
+   # def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
         """
         COMPLETAR DOCSTRING
         
@@ -52,10 +67,11 @@ class FeatureEngineeringPipeline(object):
     def run(self):
     
         df = self.read_data()
-        df_transformed = self.data_transformation(df)
-        self.write_prepared_data(df_transformed)
+        self.write_prepared_data(panda_df)
+     #   df_transformed = self.data_transformation(df)
+     #   self.write_prepared_data(df_transformed)
 
   
 if __name__ == "__main__":
-    FeatureEngineeringPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                               output_path = 'Ruta/Donde/Voy/A/Escribir/Mi/Archivo').run()
+    FeatureEngineeringPipeline(input_path = '../data/Train_BigMart.csv',
+                               output_path = '../data/Mi/dataTest').run()
